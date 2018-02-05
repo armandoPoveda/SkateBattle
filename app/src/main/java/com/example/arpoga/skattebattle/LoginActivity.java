1c1ad627b51a216923e35f514eb5e074b3725895
armandoPoveda <zote666@hotmail.com>
2018-02-05 21:54:09 +0100

    Modificado la fuente

--- app/src/main/java/com/example/arpoga/skattebattle/LoginActivity.java
+++ app/src/main/java/com/example/arpoga/skattebattle/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.arpoga.skattebattle;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+// Seteamos en una Variable donde tenemos la fuente (podemos omitir este paso y ponerla directamente cuando cargamos la fuente)
+        String carpetaFuente = "fonts/Moonlight.ttf";
+
+// Obtenemos la id del TextView donde queremos cambiar la fuente
+        TextView vistaFuenteTitulo = findViewById(R.id.textViewTitulo);
+        TextView vistaFuenteRegistro = findViewById(R.id.textView_Registrar);
+        TextView vistaFuenteLogin = findViewById(R.id.textView_Login);
+
+
+// Cargamos la fuente
+        Typeface fuente = Typeface.createFromAsset(getAssets(), carpetaFuente);
+
+// Aplicamos la fuente
+        vistaFuenteTitulo.setTypeface(fuente);
+        vistaFuenteRegistro.setTypeface(fuente);
+        vistaFuenteLogin.setTypeface(fuente);
+
     }
 }
